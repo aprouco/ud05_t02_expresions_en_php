8b5d90cd2bf8ce23bd87ecb56b35ab47cbe2f192
aprouco <adripenasrouco@gmail.com>
2022-01-14 09:25:12 +0100

    Operadores aritméticos

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>Construcción de expresións en PHP</title>
+</head>
+<body>
+    <h3>Construcción de expresións en PHP</h3>
+
+    <?php
+    //Operadores aritméticos
+    $x=10;
+    $y=3;
+    $z=$x*$y;
+    echo $x." x ".$y." = ".$z."<br/>";
+    $z=$x % $y;
+    echo $x." módulo ".$y." = ".$z."<br/>";
+    ?>
+</body>
+</html>
